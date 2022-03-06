a363991105eebfac9ec096c610716a4f82450e47
Marilyn Poku <marilynpoku@hotmail.com>
2022-03-06 10:24:01 +0000

    backend updated

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
-      console.log(data)
-    }
-    getData()
-  })
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
+            console.log(data)
+        }
+        getData()
+    })
 
-  return <h1>Hello World</h1>
+    return <h1>Hello World</h1>
 }
 
 export default App
