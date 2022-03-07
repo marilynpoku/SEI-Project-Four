19cbd4c19479a23de4e90df33b7d79656817989f
Marilyn Poku <marilynpoku@hotmail.com>
2022-03-07 12:26:59 +0000

    route and component files added

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,16 +1,40 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
+import { BrowserRouter as Routes, Route, BrowserRouter } from 'react-router-dom'
+
+
+// Components
+import NavBar from './components/common/Navbar'
+import Home from './components/pages/Home'
+import Products from './components/pages/Products'
+import SingleProduct from './components/pages/SingleProduct'
+import Brand from './components/pages/Brand'
+import Article from './components/pages/Article'
+import Profile from './components/pages/Profile'
+import CreateArticle from './components/pages/CreateArticle'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
 
 function App() {
-    useEffect(() => {
-        const getData = async () => {
-            const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
-            console.log(data)
-        }
-        getData()
-    })
 
-    return <h1>Hello World</h1>
+    return (
+        <>
+        <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/products/:id' element={<SingleProduct/>} />
+        <Route path='/brand' element={<Brand/>} />
+        <Route path='/article' element={<Article/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/createArticle' element={<CreateArticle/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        </Routes>
+        <NavBar/>
+        </BrowserRouter>
+        </>
+    )
 }
 
 export default App
---
+++ frontend/client/src/components/pages/Article.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+const Article = () => {
+
+    return (
+        <>
+        </>
+    ) 
+}
+
+export default Article
+
